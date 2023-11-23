b5247eba99117c13ce434c62cb7f49954d12549d
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-23 09:54:07 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { TopBarComponent } from './header/top-bar/top-bar.component';
+import { BottomBarComponent } from './header/bottom-bar/bottom-bar.component';
+import { SearchBarComponent } from './header/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    TopBarComponent,
+    BottomBarComponent,
+    SearchBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
