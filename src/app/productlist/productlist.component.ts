f020abd0e6a8b03a5af9829a717e1fcd2b2a899a
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-23 15:53:05 +0700

    add1product

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -5,13 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './productlist.component.html',
   styleUrls: ['./productlist.component.css']
 })
+
 export class ProductlistComponent {
     
   product = {
-    Name : "Iphone 13 pro max",
-    Price: 1300,
+    Name : "New / Iphone 15 Pro Max",
+    Price: 1400,
     Color: "Matte black",
-    Discount_Price: 50,
+    //Set discount as percentage
+    discount: 5.2,
+    instock: 14,
+    Pimg: 'assets/product-apple-iphone/IMAGE 2023-11-06 14:04:30.jpg',
   }
 
+  discountprice(){
+    return (this.product.Price*this.product.discount)/100 ;
+  }
+  stockcheck(){
+    return ( this.product.instock > 0 ? 'Only ' + this.product.instock + ' items left': 'Not in Stock' )
+  }
 }
