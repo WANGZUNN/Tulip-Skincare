26b931e9125efc1692959445eb2770717c4b941c
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-23 18:41:44 +0700

    sokheng

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -8,20 +8,32 @@ import { Component } from '@angular/core';
 
 export class ProductlistComponent {
     
-  product = {
-    Name : "New / Iphone 15 Pro Max",
+  product_1 = {
+    Name: "New / Iphone 15 Pro Max",
     Price: 1400,
     Color: "Matte black",
-    //Set discount as percentage
+    // Set discount as percentage
     discount: 5.2,
-    instock: 14,
+    instock: 13,
     Pimg: 'assets/product-apple-iphone/IMAGE 2023-11-06 14:04:30.jpg',
   }
 
-  discountprice(){
-    return (this.product.Price*this.product.discount)/100 ;
+  product_2 = {
+    Name: "Sok HengPhal",
+    Price: 10,
+    Color: "Tor innet",
+    discount: 50,
+    instock: 1,
+    Pimg: 'assets/product-apple-iphone/IMAGE 2023-11-23 18:27:45.jpg',
   }
-  stockcheck(){
-    return ( this.product.instock > 0 ? 'Only ' + this.product.instock + ' items left': 'Not in Stock' )
+
+  discountprice(product:any) {
+    // Calculate and return the discounted price
+    const discountedPrice = (product.Price * product.discount) / 100;
+    return discountedPrice;
+  }
+
+  stockcheck(product:any) {
+    return (product.instock > 0 ? 'Only ' + product.instock + ' items left' : 'Not in Stock');
   }
 }
