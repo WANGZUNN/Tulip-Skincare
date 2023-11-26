f9f91e2d148ad53be6cda5d051f20b868cef17b3
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-26 12:43:01 +0700

    grid not yet fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { BodyComponent } from './body/body.component';
 import { TopBarComponent } from './header/top-bar/top-bar.component';
 import { BottomBarComponent } from './header/bottom-bar/bottom-bar.component';
 import { SearchBarComponent } from './header/search-bar/search-bar.component';
-import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductlistComponent } from './body/productlist/productlist.component';
 import { FormsModule } from '@angular/forms';
+import { ContainerComponent } from './body/container/container.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent,
     TopBarComponent,
     BottomBarComponent,
     SearchBarComponent,
-    ProductlistComponent
+    ProductlistComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/body/productlist/productlist.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'productlist',
+  templateUrl: './productlist.component.html',
+  styleUrls: ['./productlist.component.css']
+})
+
+
+
+export class ProductlistComponent {
+  products = [
+    {
+      id: 1,
+      Name: "Winky Eye 1",
+      Price: 5,
+      Color: "Pink",
+      discount: 10,
+      instock: 14,
+      Pimg: 'assets/skincare-product/1.jpg',
+    },
+    {
+      id: 2,
+      Name: "Make up Remover",
+      Price: 8, // Corrected to a number
+      Color: "Orange",
+      discount: 50,
+      instock: 7,
+      Pimg: 'assets/skincare-product/2.jpg',
+    },
+    {
+      id: 3,
+      Name: "Make up Remover",
+      Price: 8, // Corrected to a number
+      Color: "Orange",
+      discount: 50,
+      instock: 7,
+      Pimg: 'assets/skincare-product/3.jpg',
+    }
+  ];
+
+  quantity: number[] = [0, 0]; // Initial quantity for each product
+
+  incrementQuantity(index: number) {
+    if (this.quantity[index] < this.products[index].instock) {
+      this.quantity[index]++;
+    }
+  }
+
+  decrementQuantity(index: number) {
+    if (this.quantity[index] > 0) {
+      this.quantity[index]--;
+    }
+  }
+
+  discountprice(products: any, index: number) {
+    // Calculate and return the discounted price for each product
+    const discountedPrice = (products.Price * products.discount) / 100;
+    return discountedPrice;
+  }
+
+  stockcheck(products: any) {
+    return products.instock > 0 ? 'Only ' + products.instock + ' items left' : 'Not in Stock';
+  }
+}
+
+
+
+
+
+
