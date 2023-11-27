16c29f1a223b2a63e4f8e25d3482316755673281
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-27 16:58:36 +0700

    fix griditem

--- src/app/body/productlist/productlist.component.ts
+++ src/app/body/productlist/productlist.component.ts
@@ -34,33 +34,104 @@ export class ProductlistComponent {
       Price: 14,
       Color: "Soft pink",
       discount: 20,
-      instock: 14,
+      instock: 9,
       Pimg: 'assets/skincare-product/3.jpg',
     },
     {
-      id: 3,
-      Name: "Two-way-powerder",
-      Price: 14,
-      Color: "Soft pink",
-      discount: 20,
-      instock: 14,
+      id: 4,
+      Name: "Nee-care",
+      Price: 5,
+      Color: "pink",
+      discount: 0,
+      instock: 20,
       Pimg: 'assets/skincare-product/4.jpg',
+    },
+    {
+      id: 5,
+      Name: "Vit-C+ ALA",
+      Price: 15,
+      Color: "Green ",
+      discount: 0,
+      instock: 30,
+      Pimg: 'assets/skincare-product/5.jpg',
+    },
+    {
+      id: 6,
+      Name: "GLAM & GLOW",
+      Price: 3,
+      Color: "Soft pink",
+      discount: 10,
+      instock: 100,
+      Pimg: 'assets/skincare-product/6.jpg',
+    },
+    {
+      id: 7,
+      Name: "GANYA",
+      Price: 20,
+      Color: "Green",
+      discount: 0,
+      instock: 15,
+      Pimg: 'assets/skincare-product/7.jpg',
+    },
+    {
+      id: 8,
+      Name: "Odbo",
+      Price: 10,
+      Color: "Brown",
+      discount: 0,
+      instock: 20,
+      Pimg: 'assets/skincare-product/8.jpg',
+    },
+    {
+      id: 9,
+      Name: "Triphala Plus",
+      Price: 15,
+      Color: "Green",
+      discount: 0,
+      instock: 20,
+      Pimg: 'assets/skincare-product/9.jpg',
+    },
+    {
+      id: 10,
+      Name: "Poppy Bright",
+      Price: 7,
+      Color: "Pink",
+      discount: 0,
+      instock: 20,
+      Pimg: 'assets/skincare-product/10.jpg',
+    },
+    {
+      id: 11,
+      Name: "Today's mood",
+      Price: 15,
+      Color: "Brown",
+      discount: 0,
+      instock: 25,
+      Pimg: 'assets/skincare-product/11.jpg',
+    },
+    {
+      id: 12,
+      Name: "Lemon me",
+      Price: 3,
+      Color: "light-blue",
+      discount: 0,
+      instock: 60,
+      Pimg: 'assets/skincare-product/12.jpg',
     }
   ];
 
-  quantity: number[] = [0, 0, 0, 0]; // Initial quantity for each product
+  quantity: number[] = [0, 0, 0, 0, 0,0,0,0,0,0,0,0]; // Initial quantity for each product
 
   incrementQuantity(index: number) {
     if (this.quantity[index] < this.products[index].instock) {
       this.quantity[index]++;
-      this.products[index].instock--;
+  
     }
   }
 
   decrementQuantity(index: number) {
     if (this.quantity[index] > 0) {
       this.quantity[index]--;
-      this.products[index].instock++;
       
     }
   }
