544b6da9e8a8858766286e3fad357c968e1b218c
SRUN VENG <srunveng@SRUNs-MacBook-Pro.local>
2023-11-28 15:13:46 +0700

    fix button

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,15 @@ module.exports = {
     colors: {
       'blue': '#1fb6ff',
       'purple': '#7e5bef',
-      'pink': '#ff49db',
+      'pink': '#FF1FF1',
       'orange': '#ff7849',
       'green': '#13ce66',
       'yellow': '#ffc82c',
       'gray-dark': '#273444',
       'gray': '#8492a6',
       'gray-light': '#d3dce6',
+      'discount': '#FF1611' ,
+     
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
@@ -20,7 +22,9 @@ module.exports = {
     },
     extend: {
       backgroundColor: {
-        'pink': '#ff49db',
+        'pink': '#FEC0CA',
+        'pink-500': '#FEC0CA',
+        'pink-600': '#ff1ff1',
       },
       spacing: {
         '8xl': '96rem',
@@ -30,6 +34,11 @@ module.exports = {
         '4xl': '2rem',
       },
     }
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['hover', 'focus'],
+    },
   },
   plugins: [],
 };
